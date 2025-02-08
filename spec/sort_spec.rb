611cf168d48635eab6c46069156c1bbfc1f232d4
CodeByBlazej <codebyblazej@gmail.com>
2025-02-08 08:44:33 +0000

    add test to create players method

--- spec/sort_spec.rb
+++ spec/sort_spec.rb
@@ -1,1 +1,24 @@
 require_relative '../lib/sort'
+
+describe Game do
+  describe '#select_players_names_and_colors' do
+    context 'when user gives 2 names and 2 colors' do
+      subject(:game) { described_class.new }
+      let(:players) { instance_double(Players) }
+
+      before do
+        player1_name = 'Tom'
+        player1_color = 'Red'
+        player2_name = 'Blazej'
+        player2_color = 'Green'
+        allow(game).to receive(:gets).and_return(player1_name, player1_color, player2_name, player2_color)
+        allow(game).to receive(:puts)
+      end
+
+      it 'creates 2 objects' do
+        expect(Players).to receive(:new).twice
+        game.select_players_names_and_colors
+      end
+    end
+  end
+end
