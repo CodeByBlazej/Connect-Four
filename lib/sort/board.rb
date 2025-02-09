54b786575bb2bc913d2c61d8f81fb36be26bc708
CodeByBlazej <codebyblazej@gmail.com>
2025-02-09 12:45:38 +0000

    add check row score method

--- lib/sort/board.rb
+++ lib/sort/board.rb
@@ -1,7 +1,8 @@
 require_relative '../sort'
+require 'pry-byebug'
 
 class Board
-  attr_reader :board
+  attr_reader :board, :winner
 
   def initialize board
     @board = board
@@ -12,6 +13,20 @@ class Board
       print '|'
       print row.map { |cell| cell.nil? ? ' |' : cell }.join
       puts "\n---------------"
+    end
+  end
+
+  def check_row_score(player)
+    board.each do |scores|
+      consecutive_elements = []
+
+      scores.each_cons(4) { |score| consecutive_elements << score }
+      consecutive_elements.any? do |elements|
+        if elements.all? { |el| el == player.symbol }
+          puts "#{player.name} WON THE GAME!"
+          @winner = true
+        end
+      end
     end
   end
 end
