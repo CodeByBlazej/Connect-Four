3bb1fe03eaf38353ffea86c903ba9744497b94c5
CodeByBlazej <codebyblazej@gmail.com>
2025-02-09 12:46:55 +0000

    remove players colors as its hard coded to Unicode symbols now

--- spec/sort_spec.rb
+++ spec/sort_spec.rb
@@ -1,23 +1,21 @@
 require_relative '../lib/sort'
 
 describe Game do
-  describe '#select_players_names_and_colors' do
-    context 'when user gives 2 names and 2 colors' do
+  describe '#select_players_names_and_symbols' do
+    context 'when user gives 2 names' do
       subject(:game) { described_class.new }
       let(:players) { instance_double(Players) }
 
       before do
         player1_name = 'Tom'
-        player1_color = 'Red'
         player2_name = 'Blazej'
-        player2_color = 'Green'
-        allow(game).to receive(:gets).and_return(player1_name, player1_color, player2_name, player2_color)
+        allow(game).to receive(:gets).and_return(player1_name, player2_name)
         allow(game).to receive(:puts)
       end
 
       it 'creates 2 objects' do
         expect(Players).to receive(:new).twice
-        game.select_players_names_and_colors
+        game.select_players_names_and_symbols
       end
     end
   end
