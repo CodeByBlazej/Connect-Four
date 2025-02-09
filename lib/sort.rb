0f530fd116991a341b2f6aa50d0dc97074961bd5
CodeByBlazej <codebyblazej@gmail.com>
2025-02-09 12:44:45 +0000

    rename part of the method name from colors to symbols

--- lib/sort.rb
+++ lib/sort.rb
@@ -14,23 +14,21 @@ class Game
 
   def play_game
     introduction
-    select_players_names_and_colors
+    select_players_names_and_symbols
     create_board
     @board.display_board
   end
 
-  def select_players_names_and_colors
+  def select_players_names_and_symbols
     puts "Select first player's name"
     @player1_name = gets.chomp
-    puts "Select player's color"
-    @player1_color = gets.chomp
+    @player1_symbol = "\u26AA"
     puts "Select second player's name"
     @player2_name = gets.chomp
-    puts "Select player's color"
-    @player2_color = gets.chomp
+    @player2_symbol = "\u26AB"
 
-    @player1 = Players.new(@player1_name, @player1_color)
-    @player2 = Players.new(@player2_name, @player2_color)
+    @player1 = Players.new(@player1_name, @player1_symbol)
+    @player2 = Players.new(@player2_name, @player2_symbol)
   end
 
   def create_board
