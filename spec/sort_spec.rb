0c899e6efd5c95fb9c5af37289ea5f2bb8853476
CodeByBlazej <codebyblazej@gmail.com>
2025-02-15 15:09:14 +0000

    add positive test for board_full? method

--- spec/sort_spec.rb
+++ spec/sort_spec.rb
@@ -34,14 +34,79 @@ describe Game do
     context 'when method is called and somebody wins' do
       subject(:game) { described_class.new }
       let(:board) { instance_double(Board, winner: false) }
+      let(:player1) { instance_double(Players, name: 'Tom', symbol: "\u26AA") }
+      let(:player2) { instance_double(Players, name: 'Bob', symbol: "\u26AB") }
   
       before do
         game.instance_variable_set(:@board, board)
+        game.instance_variable_set(:@player1, player1)
+        game.instance_variable_set(:@player2, player2)
+
+        allow(board).to receive(:check_row_score).with(player1).and_return(false)
+        allow(board).to receive(:check_row_score).with(player2).and_return(true)
+        allow(board).to receive(:check_column_score).with(player1).and_return(false)
+        allow(board).to receive(:check_column_score).with(player2).and_return(false)
+        allow(board).to receive(:check_diagonal_score).with(player1).and_return(false)
+        allow(board).to receive(:check_diagonal_score).with(player2).and_return(false)
+        allow(board).to receive(:check_anti_diagonal_score).with(player1).and_return(false)
+        allow(board).to receive(:check_anti_diagonal_score).with(player2).and_return(false)
+      end
+
+      it 'returns TRUE' do
+        result = game.somebody_wins?
+        expect(result).to eq(true)
+      end
+    end
+
+    context 'when method is called and nobody wins' do
+      subject(:game) { described_class.new }
+      let(:board) { instance_double(Board, winner: false) }
+      let(:player1) { instance_double(Players, name: 'Tom', symbol: "\u26AA") }
+      let(:player2) { instance_double(Players, name: 'Ben', symbol: "\u26AB") }
+
+      before do
+        game.instance_variable_set(:@board, board)
+        game.instance_variable_set(:@player1, player1)
+        game.instance_variable_set(:@player2, player2)
+
+        allow(board).to receive(:check_row_score).with(player1).and_return(false)
+        allow(board).to receive(:check_row_score).with(player2).and_return(false)
+        allow(board).to receive(:check_column_score).with(player1).and_return(false)
+        allow(board).to receive(:check_column_score).with(player2).and_return(false)
+        allow(board).to receive(:check_diagonal_score).with(player1).and_return(false)
+        allow(board).to receive(:check_diagonal_score).with(player2).and_return(false)
+        allow(board).to receive(:check_anti_diagonal_score).with(player1).and_return(false)
+        allow(board).to receive(:check_anti_diagonal_score).with(player2).and_return(false)
+      end
+
+      it 'returns FALSE' do
+        result = game.somebody_wins?
+        expect(result).to eq(false)
+      end
+    end
+  end
+
+  describe '#board_full?' do
+    context 'when board is full' do
+      subject(:game) { described_class.new }
+      let(:board) { instance_double(Board) }
+      let(:board_array) { Array.new(6) { Array.new(7) } }
+      let(:player1_symbol) { "\u26AA" }
+
+      before do
+        game.instance_variable_set(:@board, board)
+        game.instance_variable_set(:@player1_symbol, player1_symbol)
+
+        allow(board).to receive(:board).and_return(board_array)
+
+        board.board.map! do |row|
+          row.map { |el| el = player1_symbol}
+        end
       end
 
       it 'returns true' do
-        result = game.somebody_wins?
-        expect(result).to eq(false)
+        result = game.board_full?
+        expect(result).to eq(true)
       end
     end
   end
